73c3bbfc0fad0f58806b4a939a882dd170d7737d
zoowx321 <zoowx321@naver.com>
2016-11-09 00:53:00 -0800

    Now It can make count variable dynamically using comp_GetMax function

--- src/decision_tree.java
+++ src/decision_tree.java
@@ -4,6 +4,11 @@ import java.io.*;
 
 public class decision_tree 
 {
+	
+	static int width = 5;
+	static int height = 14;
+	static double[][] count;
+	static int Old;
 	public static void main(String[] args) throws Exception
 	{
 		GetData();
@@ -13,8 +18,7 @@ public class decision_tree
 		String GetLine;
 		int j = 0;
 		int i = 0;
-		int width = 5;
-		int height = 14;
+		
 		//ArrayList<String> Attribute = new ArrayList<String>();
 		//int NumOfAttribute = 5;
 		HashMap<Integer,ArrayList<String>> SaveAttribute = new HashMap<Integer,ArrayList<String>>();
@@ -44,9 +48,20 @@ public class decision_tree
 			System.out.println();
 		}
 		/////////save attribute///////////////
+		/*
+		 key1	ArrayList(Attribute 11, Attribute 12, Attribute 13...)
+		 key2	ArrayList(Attribute 21, Attribute 22, ........)
+		 key3
+		 ...
+		 */
+		Old = 0;
+		int NumAtt = 0;
+		int MaxNumAttVal = 0;
 		for(i = 0; i<width;i++)
 		{
 			System.out.println();
+			int NumAttval = 0;
+			
 			for(j = 0;j<height;j++)
 			{
 				if(j>0)
@@ -57,17 +72,76 @@ public class decision_tree
 					{
 						SaveAttributeTmp.add(save[j][i]);
 						System.out.print(save[j][i]+" ");
+						NumAttval++;
 					}
 				}
 				else if(j == 0)
 				{
 					SaveAttributeTmp.add(save[j][i]);
 					System.out.print(save[j][i]+" ");
+					NumAttval++;
 				}
+				//count_Att(save[j][i],SaveAttributeTmp);
+				
 			}
-			
+			NumAtt++;
+			//System.out.println(" NumAttVal " + NumAttval);
+			MaxNumAttVal = comp_GetMax(NumAttval);
+			//System.out.println(" Temp MaxNumAttVal " + MaxNumAttVal);
 			SaveAttribute.put(i,SaveAttributeTmp);
-		}
+			
+		}	
+		count = new double[NumAtt][MaxNumAttVal];
+		//System.out.println("NumAtt "+NumAtt + " MaxNumAttVal " + MaxNumAttVal);
 	}
+	static int comp_GetMax(int New)
+	{
+		//System.out.println("Old New" + Old + " " + New);
+		if (Old<=New)
+		{
+			Old = New;
+			return New;
+			
+			//System.out.println(winner);
+		}
+		else if (Old>New)
+		{
+			return Old;
+		}
+		return 0;
+		
+	}
+	//SaveAttribute.
+	/*static void count_Att(String save, ArrayList<String> SaveAttributeTmp)
+	{
+		if(save[i][0].equals("Sunny"))
+			count[0][0]++;
+		else if(save[i][0].equals("Overcast"))
+			count[0][1]++;
+		else if(save[i][0].equals("Rain"))
+			count[0][2]++;
+		
+		if(save[i][1].equals("Hot"))
+			count[1][0]++;
+				else if(save[i][1].equals("Mild"))
+					count[1][1]++;
+						else if(save[i][1].equals("Cool"))
+							count[1][2]++;
+								
+		
+		if(save[i][2].equals("High"))
+			count[2][0]++;
+				else if(save[i][2].equals("Normal"))
+					count[2][1]++;
+						
+		
+		if(save[i][3].equals("Weak"))
+			count[3][0]++;
+				else if(save[i][3].equals("Strong"))
+					count[3][1]++;
+						
+		c++;
+
+	}*/
 
 }
