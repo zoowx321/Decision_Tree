a5a5a94c0ffaa64a31f021cc737423c2f03ada68
zoowx321 <zoowx321@naver.com>
2016-11-13 06:23:18 -0800

    finish getting weighted average entropy and its minimum closes #5

--- src/decision_tree.java
+++ src/decision_tree.java
@@ -9,7 +9,11 @@ public class decision_tree
 	static int height = 14;
 	static int[][][] CountAtt;
 	static int[] CountClass;
+	static double[][] H;
+	static double[] WeightedAverageH;
 	static int Old;
+	static int Id = 0;
+	static double old;
 	public static void main(String[] args) throws Exception
 	{
 		GetData();
@@ -117,8 +121,10 @@ public class decision_tree
 		}
 		CountAtt = new int[NumAtt][MaxNumAttVal][SaveClass.get(0).size()];
 		CountClass = new int[SaveClass.get(0).size()];
+		
 		count_Att(NumAtt, MaxNumAttVal, save, SaveAttribute, SaveClass);
 		
+		//CalculateEntropy();
 		//System.out.println(" 0:" + SaveAttribute.get(0));
 		//System.out.println(" have sunny?:" + SaveAttribute.get(0).contains("sunny"));
 		////////count = new double[NumAtt][MaxNumAttVal];
@@ -128,7 +134,8 @@ public class decision_tree
 	}
 	static double CalculateEntropy()
 	{
-		count_Att();
+		
+		
 		return 0.0;
 	}
 	static void count_Att(int NumAtt, int MaxNumAttVal, String[][] save, ArrayList<ArrayList<String>> SaveAttribute, ArrayList<ArrayList<String>> SaveClass)
@@ -164,20 +171,121 @@ public class decision_tree
 				
 			}
 		}
-		
+		int num = 0;
+		int num4weight = 0;
+		H = new double[NumAtt][MaxNumAttVal];
+		WeightedAverageH = new double[NumAtt];
 		for(int i = 0; i<width-1;i++)
 		{
-			
+			if(ContainAlpha(SaveAttribute.get(i).get(0)))
+			{
 				for(int j = 0; j<SaveAttribute.get(i).size();j++)
 				{
+					num = 0;
 					for(int n = 0; n<SaveClass.get(0).size();n++)
 					{
 						System.out.println("saveAttribute" + SaveAttribute.get(i).get(j)+" "+SaveClass.get(0).get(n) +" CountAtt["+i+"]["+j+"]["+n+"] "+CountAtt[i][j][n]);
+						num += CountAtt[i][j][n];
+						
 					}
+					
+					
+						for(int n = 0; n<SaveClass.get(0).size();n++)
+						{
+							if((double)CountAtt[i][j][n]/num == 0)
+							{
+								H[i][j] = 0;
+								break;
+							}
+							H[i][j] += -(double)CountAtt[i][j][n]/num*CalLog2((double)CountAtt[i][j][n]/num);
+						}
+						//System.out.println("H "+H[i][j]);
+						WeightedAverageH[i] += (double)H[i][j]*num;
+						num4weight += num;
+					
+					
 				}
+				WeightedAverageH[i] = (double)WeightedAverageH[i]/num4weight;
+				System.out.println("H " + WeightedAverageH[i]);
+			}
+			else
+			{
+				old = 1;//cause it's min..... not 0
+				double Min = 0;
+				for(int x = 0; x<SaveAttribute.get(i).size();x++)
+				{
+					for(int n = 0; n<SaveClass.get(0).size();n++)
+					{
+						System.out.println("saveAttribute" + SaveAttribute.get(i).get(x)+" "+SaveClass.get(0).get(n) +" CountAtt["+i+"]["+x+"]["+n+"] "+CountAtt[i][x][n]);
+						
+					}
+					int[] CountAttBelow = new int [SaveClass.get(0).size()];
+					int[] CountAttAbove = new int [SaveClass.get(0).size()];
+					double H_T1 = 0;
+					double H_T2 = 0;
+					double H_T = 0;
+						int num1 = 0;
+						int num2 = 0;
+						
+						for(int y1 = 0; y1<=x;y1++)
+						{
+							for(int n = 0; n<SaveClass.get(0).size();n++)
+							{
+								CountAttBelow[n] += CountAtt[i][y1][n];
+								num1 += CountAttBelow[n];
+							}
+						}
+						
+						
+						for(int y2 = x; y2<SaveAttribute.get(i).size();y2++)
+						{
+							for(int n = 0; n<SaveClass.get(0).size();n++)
+							{
+								CountAttAbove[n] += CountAtt[i][y2][n];
+								num2 += CountAttAbove[n];
+							}
+						}
+						
+						for(int n = 0; n<SaveClass.get(0).size();n++)
+						{
+							System.out.println("below "+CountAttBelow[n] + " " + num1);
+							System.out.println("above "+CountAttAbove[n] + " " + num2);
+							
+							H_T1 += -(double)CountAttBelow[n]/num1*CalLog2((double)CountAttBelow[n]/num1);
+							H_T2 += -(double)CountAttAbove[n]/num2*CalLog2((double)CountAttAbove[n]/num2);
+							if(CountAttBelow[n] == 0)
+							{
+								H_T1 = 0;
+							}
+							if(CountAttAbove[n] == 0)
+							{
+								H_T2 = 0;
+							}
+						}
+						//System.out.println("H_temp1 2 :" + H_T1 + " "+H_T2);
+						H_T = (double)num1/(num1+num2)*H_T1 + (double)num2/(num1+num2)*H_T2; 
+						//System.out.println("H_temp" + H_T);
+						Min = comp(H_T,x);
+						
+				}
+				WeightedAverageH[i] = Min;
+				System.out.println("H " + WeightedAverageH[i] + " MIn " + Min);
+			}
 			
 		}
-		System.out.println(CountClass[0] + " " + CountClass[1]);
+		double MinWeightedAverageH = 0;
+		for(int i = 0; i<width-1;i++)
+		{
+			if(i == 0)
+				old = WeightedAverageH[i];
+			MinWeightedAverageH = comp(WeightedAverageH[i],i);
+		}
+		//System.out.println(CountClass[0] + " " + CountClass[1]);
+		System.out.println("MinWeightedAverageH : "+SaveAttribute.get(Id) + " " + MinWeightedAverageH);
+	}
+	static double CalLog2(double a)
+	{
+		return Math.log(a)/Math.log(2);
 	}
 	static boolean ContainAlpha(String OneOfAtt)
 	{
@@ -197,6 +305,25 @@ public class decision_tree
 		else if (Old>New)
 		{
 			return Old;
+		}
+		return 0;
+		
+	}
+	
+	static double comp(double New, int id)
+	{
+		//System.out.println("Old New" + Old + " " + New);
+		if (old>=New)
+		{
+			old = New;
+			Id = id;
+			return New;
+			
+			//System.out.println(winner);
+		}
+		else if (old<New)
+		{
+			return old;
 		}
 		return 0;
 		
