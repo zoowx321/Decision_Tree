650e2ae349c111da93742f34e5175a11ebf3618f
soongeun Hwang <soongeun Hwang>
2016-11-16 00:43:00 +0900

    decompose 구현중, refs #7

--- src/decision_tree.java
+++ src/decision_tree.java
@@ -17,6 +17,8 @@ public class decision_tree
 	static int width = 5;
 
 	static int height = 14;
+	
+	static String[][] save = new String[height][width];
 
 	static int[][][] CountAtt;
 
@@ -32,18 +34,167 @@ public class decision_tree
 
 	static double old;
 
-	//static TreeNode root;
+	static TreeNode root = new TreeNode();
 
+	static DataInfo info;
 	public static void main(String[] args) throws Exception
 
 	{
 
+		GetData();
+		//System.out.println(calculation(info.NumAtt, info.MaxNumAttVal, info.save, info.SaveAttribute, info.SaveClass));
+		root.decompositionAttribute = calculation(info.NumAtt, info.MaxNumAttVal, info.save, info.SaveAttribute, info.SaveClass);
+		root.Istravel = true;
+		//System.out.println(root.decompositionAttribute.size());
+		Decompose(root);
+	}
+	static int count = 0;
+	public static void Decompose(TreeNode node) throws Exception
+	{
+		//System.out.println("fadsf"+root.remainAttribute);
+		System.out.println("count : "+ count);
+		if(count>0)
+		{
+			GetDataChild(node);
+			node.decompositionAttribute = calculation(info.NumAtt, info.MaxNumAttVal, info.save, info.SaveAttribute, info.SaveClass);
+			if(info.entropy>0.7)
+				return;
+			node.Istravel = true;
+			System.out.println("test");
+		}
+		node.children = new TreeNode[node.decompositionAttribute.size()];
+		
+		ArrayList<ArrayList<String>> tmp = new ArrayList<ArrayList<String>>();
+		ArrayList<String> tmpIn = new ArrayList<String>();
+		tmpIn.add("");
+		tmp = node.remainAttribute;
+		tmp.set(Id,tmpIn);
+		
+		System.out.println("children data:");
+		for(int i = 0; i<node.decompositionAttribute.size();i++)
+		{
+			node.children[i] = new TreeNode();
+			node.children[i].parent = node;
+			node.children[i].data.addAll(MatchString(save, node.decompositionAttribute.get(i),Id));
+			node.children[i].decompositionValue = node.decompositionAttribute.get(i);
+			
+			node.children[i].remainAttribute = tmp;
+			node.children[i].remainClass = node.remainClass;
+			System.out.println(node.children[i].data);
+		}
+		System.out.println(node.remainAttribute);
+		System.out.println(node.children[0].remainAttribute);
+		System.out.println();
+		/*for(int i = 0; i<node.decompositionAttribute.size();i++)
+		{
+			Decompose(node.children[i]);
+			count++;
+		}*/
+		if(count < 2)
+		{
+			count++;
+			Decompose(node.children[0]);
+			
+			//System.out.println("test" + count);
+		}
+		
+	}
+	public static ArrayList<ArrayList<String>> MatchString(String[][] input, String matchingTarget,int Att)
+	{
+		ArrayList<ArrayList<String>> output = new ArrayList<ArrayList<String>>();
+		ArrayList<String> tmp;
+
+		for(int i = 0; i<input.length;i++)
+		{
+			tmp = new ArrayList<String>();
+			if(input[i][Att].equals(matchingTarget))
+			{
+				for(int j = 0; j<input[i].length;j++)
+				{
+					tmp.add(input[i][j]); 
+				}
+
+				//System.out.println("tmp : "+tmp);
+				output.add(tmp);
+			}
+			
+			
+		}
+		//System.out.println("output: "+ output);
+		return output;
+	}
+	public static void GetDataChild(TreeNode node) throws Exception
+	{
+		ArrayList<ArrayList<String>> input = new ArrayList<ArrayList<String>>();
+		input = node.data;
+		save = new String[input.size()][width];
+		int j = 0;
+
+		int i = 0;
+
+		ArrayList<ArrayList<String>> SaveAttribute = new ArrayList<ArrayList<String>>();
+		SaveAttribute = node.remainAttribute;
+		ArrayList<ArrayList<String>> SaveClass = new ArrayList<ArrayList<String>>();
+		SaveClass = node.remainClass;
+
+		
+		for(i = 0; i<input.size();i++)
+
+		{
+
+			for(j = 0;j<width;j++)
+
+			{
+
+				save[i][j] = input.get(i).get(j);
+
+			}
+
+			//System.out.println();
+
+		}
+
+
+
+		Old = 0;
+
+		int NumAtt = SaveAttribute.size();
+
+		int MaxNumAttVal = 0;
+		for(i = 0; i<SaveAttribute.size();i++)
+		{
+			MaxNumAttVal = comp_GetMax(SaveAttribute.get(i).size());
+		}
 		
 
-		GetData();
+		CountAtt = new int[NumAtt][MaxNumAttVal][SaveClass.get(0).size()];
 
+		CountClass = new int[SaveClass.get(0).size()];
+
+		info = new DataInfo();
+		info.NumAtt = NumAtt;
+		info.MaxNumAttVal = MaxNumAttVal;
+		info.save = save;
+		info.SaveAttribute = SaveAttribute;
+		info.SaveClass = SaveClass;
+		//delete    System.out.println(calculation(info.NumAtt, info.MaxNumAttVal, info.save, info.SaveAttribute, info.SaveClass));
+
+		
+
+		//CalculateEntropy();
+
+		//System.out.println(" 0:" + SaveAttribute.get(0));
+
+		//System.out.println(" have sunny?:" + SaveAttribute.get(0).contains("sunny"));
+
+		////////count = new double[NumAtt][MaxNumAttVal];
+
+		System.out.println();
+
+		//System.out.println(" test:" + SaveAttribute.get(3));
+
+		//System.out.println("NumAtt "+NumAtt + " MaxNumAttVal " + MaxNumAttVal);
 	}
-
 	public static void GetData() throws Exception
 
 	{
@@ -66,7 +217,7 @@ public class decision_tree
 
 		ArrayList<String> SaveAttributeTmp = new ArrayList<String>();
 
-		String[][] save = new String[height][width];
+		//String[][] save = new String[height][width];
 
 		//String[][] SaveAttribute = new String[NumOfAttribute][];
 
@@ -114,19 +265,7 @@ public class decision_tree
 
 		}
 
-		/////////save attribute///////////////
 
-		/*
-
-		 key1	ArrayList(Attribute 11, Attribute 12, Attribute 13...)
-
-		 key2	ArrayList(Attribute 21, Attribute 22, ........)
-
-		 key3
-
-		 ...
-
-		 */
 
 		Old = 0;
 
@@ -237,7 +376,6 @@ public class decision_tree
 			}
 
 			
-
 			
 
 			
@@ -245,14 +383,21 @@ public class decision_tree
 			//SaveAttributeTmp.clear();
 
 		}
-
+		root.remainAttribute = new ArrayList<ArrayList<String>>();
+		root.remainAttribute.addAll(SaveAttribute);
+		root.remainClass = new ArrayList<ArrayList<String>>();
+		root.remainClass.addAll(SaveClass);
 		CountAtt = new int[NumAtt][MaxNumAttVal][SaveClass.get(0).size()];
 
 		CountClass = new int[SaveClass.get(0).size()];
 
-		
-
-		count_Att(NumAtt, MaxNumAttVal, save, SaveAttribute, SaveClass);
+		info = new DataInfo();
+		info.NumAtt = NumAtt;
+		info.MaxNumAttVal = MaxNumAttVal;
+		info.save = save;
+		info.SaveAttribute = SaveAttribute;
+		info.SaveClass = SaveClass;
+		//delete    System.out.println(calculation(info.NumAtt, info.MaxNumAttVal, info.save, info.SaveAttribute, info.SaveClass));
 
 		
 
@@ -272,19 +417,8 @@ public class decision_tree
 
 	}
 
-	static double CalculateEntropy()
 
-	{
-
-		
-
-		
-
-		return 0.0;
-
-	}
-
-	static void count_Att(int NumAtt, int MaxNumAttVal, String[][] save, ArrayList<ArrayList<String>> SaveAttribute, ArrayList<ArrayList<String>> SaveClass)
+	static ArrayList<String> calculation(int NumAtt, int MaxNumAttVal, String[][] save, ArrayList<ArrayList<String>> SaveAttribute, ArrayList<ArrayList<String>> SaveClass)
 
 	{
 
@@ -292,7 +426,7 @@ public class decision_tree
 
 		{
 
-			for(int j = 0; j<height;j++)
+			for(int j = 0; j<save.length;j++)
 
 			{
 
@@ -578,7 +712,8 @@ public class decision_tree
 		//System.out.println(CountClass[0] + " " + CountClass[1]);
 
 		System.out.println("MinWeightedAverageH : "+SaveAttribute.get(Id) + " " + MinWeightedAverageH);
-
+		DataInfo.entropy = MinWeightedAverageH;
+		return SaveAttribute.get(Id);
 	}
 
 	static double CalLog2(double a)
