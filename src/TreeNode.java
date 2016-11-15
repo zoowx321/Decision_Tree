40d479a24011aee0f7f7ee044bc9e8b61f5e73a3
soongeun Hwang <soongeun Hwang>
2016-11-16 00:41:36 +0900

    class안에 remainAttribute, remainClass,Att를 멤버 번수로 추가

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,25 @@
+package hw9;
+import java.util.ArrayList;
 
-public class TreeNode {
+
+class TreeNode 
+{
+	public int usedAttribute;
+	public int entropy;
+	boolean Istravel;
+	public ArrayList<ArrayList<String>> data;
+	public ArrayList<ArrayList<String>> remainAttribute;
+	public ArrayList<ArrayList<String>> remainClass;
+	public int Att;
+	public ArrayList<String> decompositionAttribute;
+	public String decompositionValue;
+	
+	public TreeNode[] children;
+	public TreeNode parent;
+	
+	public TreeNode() {
+		data = new ArrayList<ArrayList<String>>();
+		
+	}
 
 }
