0912a6479a2978b7761bcc9c69b053227ec3ab02
soongeun Hwang <soongeun Hwang>
2016-11-16 00:44:25 +0900

    데이터의 정보를 효율적으로 저장하기 위해 만든 class

---
+++ src/DataInfo.java
@@ -0,0 +1,13 @@
+package hw9;
+
+import java.util.ArrayList;
+
+public class DataInfo 
+{
+	public int NumAtt;
+	public int MaxNumAttVal;
+	public String[][] save;
+	public ArrayList<ArrayList<String>> SaveAttribute;
+	public ArrayList<ArrayList<String>> SaveClass;
+	public static double entropy;
+}
